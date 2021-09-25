8397fdae8b43cb3adfb3e182c0dd567c708f7420
Carlos Figueroa Soto <figueroasotocarlos@gmail.com>
2021-09-25 01:07:51 -0500

    Noticias terminado

--- proyecto-adminconjuntos/src/components/NoticiasPostAdmin.vue
+++ proyecto-adminconjuntos/src/components/NoticiasPostAdmin.vue
@@ -9,12 +9,33 @@
                 >
                 <v-list-item three-line>
                     <v-list-item-content>
+                    <v-flex class="row  mt-3 align-center">
+                        <div>
+                            <v-list-item-title class="text-h5 ml-3">
+                                Lorem Ipsum
+                            </v-list-item-title>
+                        </div>
+                        <v-spacer></v-spacer>
+                        <div >
+                            <v-btn
+                            color="Black"
+                            icon
+                            >
+                                <v-icon>mdi-application-edit</v-icon>
+                            </v-btn>
+                            <v-btn
+                            color="Black"
+                            icon
+                            >
+                                <v-icon>mdi-delete</v-icon>
+                            </v-btn>
+                        
+                        </div>
+                        
+                    </v-flex>
                     
-                    <v-list-item-title class="text-h5 mt-3">
-                        Lorem Ipsum
-                    </v-list-item-title>
                     <v-list-item-subtitle>24/09/2021 2:17</v-list-item-subtitle>
-                    <div class="e mb-3 mt-4">
+                    <div class=" mb-3 mt-4">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras commodo metus tellus, eget sagittis tellus sollicitudin ac. Sed ultrices, neque ut condimentum gravida, justo turpis vestibulum eros, id posuere enim odio at enim. Integer ultricies libero dui, vitae blandit nisi aliquet nec. Integer feugiat pulvinar mollis.
                     </p> 
                     </div>
--- proyecto-adminconjuntos/src/views/diseño.vue
+++ proyecto-adminconjuntos/src/views/diseño.vue
@@ -62,7 +62,7 @@
                 Apellido:
             </div>
             <hr color="gray" class=" ml-2  mr-2" >
-            <v-flex>
+            <v-flex class="ml-0">
                 <v-list class=""
                 dense
                 >
@@ -70,23 +70,102 @@
                     v-for="item in items"
                     :key="item.title"
                     link
+                    class="mt-4"
                 >
                     <v-list-item-icon>
+                        
                         <v-icon
-                        align="left"
+                        align="center"
                         large
+                        class=""
                         color="white">
                         {{ item.icon }}
                         </v-icon>
-                    </v-list-item-icon>
+                        
 
+                    </v-list-item-icon>
                     <v-list-item-content>
                         <v-list-item-title
-                        class="ml-7 white--text text-h6 ">
+                        class="titulo_item white--text text-h6"
+                        
+                        >
+                        
                         {{ item.title }}
                         </v-list-item-title>
+                        
                     </v-list-item-content>
+                    
+                    <div v-if="(item.title=='Noticias')">
+                        <v-dialog
+                        
+                        v-model="dialog"
+                        width="400px"
+                        flat
+                        >
+                        <template v-slot:activator="{ on, attrs }">
+                            <v-btn
+                                class="boton_noticias"
+                                color="white"
+                                icon
+                                v-bind="attrs"
+                                v-on="on"
+                                >
+                                    <v-icon>mdi-plus</v-icon>
+                            </v-btn>
+                            </template>
+                            <v-card class="dialogo"
+                            color="#EDE7D9">
+                                    <v-card-title>
+                            <span class="text-h5">Escriba su noticia</span>
+                            </v-card-title>
+                            
+                            <v-text-field
+                            label="Titulo*"
+                            class="ma-5"
+                            required
+                            ></v-text-field>
+                            <v-divider class="mr-5 ml-5" ></v-divider>
+                            <v-textarea
+                            class="mr-5 ml-5"
+                            v-model="title"
+                            label="Noticia"
+                            counter
+                            maxlength="200"
+                            full-width
+                            single-line
+                            m
+                            ></v-textarea>
+                            
+                            
+                            <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn
+                                elevation="2"
+                                rounded
+                                color="purple"
+                                class="white--text"
+                                @click="dialog = false"
+                            >
+                                Cancelar
+                            </v-btn>
+                            <v-btn
+                                elevation="2"
+                                rounded
+                                color="purple"
+                                class="white--text"
+                                @click="dialog = false"
+                                >
+                                    Publicar
+                            </v-btn>
+
+                            </v-card-actions>
+                            
+                            
+                        </v-card>
+                        </v-dialog>
+                    </div>
                 </v-list-item>
+                
                 <br>
                 </v-list>
             </v-flex>
@@ -105,8 +184,16 @@
 
         
     </v-navigation-drawer>
-                <noticias-post/>
-                
+        <div>
+            <noticias-post/>
+        </div>
+        <div>
+            <noticias-post/>
+        </div>
+        <div>
+            <noticias-post/>
+        </div>
+
         </div>
                 
 
@@ -126,6 +213,18 @@
 .titulo{
     font-size: 70px;
 }
+.titulo_item{
+    float: unset;
+    margin-top: 1%;
+}
+.dialogo{
+    background-color: #EDE7D9;
+/* purple */
+
+border: 5px solid #440381;
+box-sizing: border-box;
+border-radius: 15px;
+}
 </style>
 
 <script>
@@ -136,6 +235,7 @@ import NoticiasPost from '../components/NoticiasPost.vue';
         name: 'App',
         data() {
             return{
+                dialog: false,
                 drawer:true,
                 items: [
                 { title: 'Noticias', icon: 'mdi-newspaper-variant' },
@@ -145,6 +245,7 @@ import NoticiasPost from '../components/NoticiasPost.vue';
             };
         }
     };
+
 </script>
 
 
