fd208f686b8c2517e61031ffab35311f5a8ea2e5
Juanda95 <arq.juandavidmunoz@gmail.com>
2021-10-08 22:59:51 -0500

    eliminacion

--- Proyecto_adminconjuntos_Back/src/model/NoticiasModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const NoticiaSchema = mongoose.Schema({
-  titulo: String,
-  cuerpo: String,
-  fecha: {type:Date, default: Date.now}
-});
-
-module.exports = mongoose.model("Noticia", NoticiaSchema);
