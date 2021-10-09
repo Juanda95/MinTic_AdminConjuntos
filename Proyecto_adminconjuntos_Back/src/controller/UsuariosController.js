c595a46ecaf622afa07379efa7128d26a6fa46f2
Carlos Figueroa Soto <figueroasotocarlos@gmail.com>
2021-10-08 21:53:08 -0500

    Noticias, contaduria y propietarios conectado al back

--- Proyecto_adminconjuntos_Back/src/controller/UsuariosController.js
+++ Proyecto_adminconjuntos_Back/src/controller/UsuariosController.js
@@ -1,4 +1,4 @@
-const Usuario = require("../model/UsuariosModel");
+const Usuario = require("../model/UsuariosModel")
 
 module.exports = class UsuariosModel {
   static async getAll(request, response) {
@@ -6,61 +6,53 @@ module.exports = class UsuariosModel {
       const result = await Usuario.find();
       response.status(200).json(result);
     } catch (err) {
-      response.status(404).json({ message: err.message });
+      response.status(404).json({message: err.message});
     }
   }
   static async getById(request, response) {
     try {
       const id = request.params.id;
-      const result = await Usuario.findOne({ cedula: id });
+      const result = await Usuario.findOne({cedula: id});
       if (result != null) {
-        response.status(200).json(result);
+        response.status(200).json(result)
       } else {
         response.status(404).json();
       }
     } catch (err) {
-      response.status(400).json({ message: err.message });
+      response.status(400).json({message: err.message})
     }
   }
 
-  static async deleteById(request, response) {
+  static async deleteById(request, response){
     try {
       const id = request.params.id;
-      await Usuario.deleteOne({ cedula: id });
+      await Usuario.deleteOne({cedula: id});
       response.status(200).json();
     } catch (err) {
-      response.status(400).json({ message: err.message });
+      response.status(400).json({message: err.message})
     }
   }
 
-  static async create(request, response) {
+  static async create(request, response){
     try {
-      if (!request.body.cedula || !request.body.password) {
-        response.json({
-          success: false,
-          msg: "El usuario y la contraseña son obligatorios!",
-        });
-      } else {
-        const newUsuario = new Usuario (request.body)
-        newUsuario.save(function(err){
-          if(err) return response.json({success: false,msg:'la cedula del usuario ya existe!'})
-          return response.json({success: true,msg:'usuario registrado!'})
-        })
-      }
+      const document = request.body;
+      const newRestaurant = await Usuario.create(document);
+      response.status(201).json(newRestaurant);
     } catch (err) {
-      response.status(400).json({ message: err.message });
+      response.status(400).json({message: err.message})
     }
   }
 
   static async updateById(request, response) {
     try {
-      const id = request.params.id;
+      const _id = request.params.id;
       const val = request.params.val;
       const document = request.body;
-      await Usuario.updateOne({ cedula: id }, { $set: { borough: val } });
-      response.status(200).json();
+      const respuesta= await Usuario.findByIdAndUpdate(_id,document,{new:true});
+      //await Usuario.updateOne({"cedula": id}, {$set: {"borough": val}});
+      response.status(200).json(respuesta);
     } catch (err) {
-      response.status(400).json({ message: err.message });
+      response.status(400).json({message: err.message})
     }
   }
-};
+}
