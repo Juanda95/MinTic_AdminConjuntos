99a8c6f5c980d2da43ff488ca15d255ad354d9e6
Juanda95 <arq.juandavidmunoz@gmail.com>
2021-10-12 18:26:32 -0500

    ajuste url heroku

--- proyecto-adminconjuntos_From/src/main.js
+++ proyecto-adminconjuntos_From/src/main.js
@@ -10,7 +10,7 @@ import VueAxios from 'vue-axios'
 Vue.use(VueAxios, axios) 
 
 // Agregamos la URL base de nuestra API 
-axios.defaults.baseURL = 'http://localhost:2000/api';
+axios.defaults.baseURL = 'https://admin-conjuntos.herokuapp.com/api';
 
 new Vue({
   router,
