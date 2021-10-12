91c038ba93b9c27f4fe41d3d83302fa25a8bfa93
Juanda95 <arq.juandavidmunoz@gmail.com>
2021-10-11 23:41:45 -0500

    se cambia el puerto co

--- Proyecto_adminconjuntos_Back/app.js
+++ Proyecto_adminconjuntos_Back/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-
+app.use(cors());
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 });
 
 const app = express();
-// app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
