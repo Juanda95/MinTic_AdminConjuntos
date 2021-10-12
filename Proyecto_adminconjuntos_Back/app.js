744aa37bf6f8b33c0b80229d83d04e2b31439c00
Juanda95 <arq.juandavidmunoz@gmail.com>
2021-10-11 23:45:23 -0500

    se cambia el puer

--- Proyecto_adminconjuntos_Back/app.js
+++ Proyecto_adminconjuntos_Back/app.js
@@ -2,18 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-app.use(cors());
-// Configurar cabeceras y cors
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
@@ -35,5 +26,5 @@ mongoose.connect(process.env.URI_DB)
 
 const port = process.env.PORT;
 app.listen(port, () => 
-  console.log(`La aplicación esta escuchando en http://127.0.0.1:${port}`)
+  console.log(`La aplicación esta escuchando en http://localhost:${port}`)
 )
