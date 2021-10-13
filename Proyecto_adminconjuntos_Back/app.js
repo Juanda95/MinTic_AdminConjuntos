09d9824f8d9c5d2ddba4d5bdaf7f50b6786d9fbd
Juanda95 <arq.juandavidmunoz@gmail.com>
2021-10-12 20:10:51 -0500

    final

--- Proyecto_adminconjuntos_Back/app.js
+++ Proyecto_adminconjuntos_Back/app.js
@@ -2,21 +2,26 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "site")))
+
 app.use(express.urlencoded({extended: true}))
 
+// rutas
+
+
 if (process.env.NODE_ENV === 'production'){
+  app.use("/api", require("./src/routes/routes"))
   app.use(express.static(__dirname+'/site'))
   app.use('*',(req,res)=>{
     res.sendFile(__dirname + '/site/index.html')
   })
 }
 
-// rutas
-app.use("/api",require("./src/routes/routes"));
 
 // DB
 const mongoose = require("mongoose");
